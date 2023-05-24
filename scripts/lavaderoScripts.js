9d26b9fef1d7e8730adeb7579b9a58d7b16beff0
nicolezbur <134172423+nicolezbur@users.noreply.github.com>
2023-05-24 12:35:37 -0300

    Update lavaderoScripts.js

--- scripts/lavaderoScripts.js
+++ scripts/lavaderoScripts.js
@@ -5,46 +5,42 @@ function registrarCliente() {
     alert("Se ha registrado crorrectamente a " + usuario + " " + apellido + ", se le enviara un mensaje a su casilla de correo electronico " + correo)
 }
 
-function registrarUsuario(){
-    
+function registrarUsuario() {
+    var usuario = document.getElementById("usuario").value;
     alert("Se tomó correctamente el registro del usuario " + usuario);
 }
 
-function clave(){
-    alert(blablabla)
-}
-
-function validarClave(){
+function validarClave() {
     let clave1 = document.getElementById("clave1").value;
     let clave2 = document.getElementById("clave2").value;
 
     if (clave1 == clave2) {
-       alert("Ambas contraseñas coinciden");
-       return true;
+        alert("Ambas contraseñas coinciden");
+        return true;
     } else {
-       alert("Ambas contraseñas deben coincidir");
-       return false;
+        alert("Ambas contraseñas deben coincidir");
+        return false;
     }
 }
 
 function registrarUsuarios() {
     if (confirm("\u00BFEst\u00E1 seguro de enviar el formulario?")) {
 
-		let resulClave= validarClave(document.getElementById('clave1').value);
+        let resulClave = validarClave(document.getElementById('clave1').value);
         let nombreUsuario = document.getElementById('nombreUsuario').value;
 
-		if (resulClave == true) {
-			alert("Se tomó correctamente el registro del usuario " + nombreUsuario);
-            return true;    
-		}		
-	}
-	else {
+        if (resulClave == true) {
+            alert("Se tomó correctamente el registro del usuario " + nombreUsuario);
+            return true;
+        }
+    }
+    else {
         alert("Las contraseñas no coinciden");
-		return false;
-	}
+        return false;
+    }
 }
 
-function registrarVehiculo(){
+function registrarVehiculo() {
     let marca = document.getElementById("marca").value;
     let modelo = document.getElementById("modelo").value;
     let patente = document.getElementById("patente").value;
